e2fc5ace9a023969c661eb3429da137657062f16
Animsh Basak Chowdhury <animeshbchowdhury@gmail.com>
2024-03-25 23:50:28 -0400

    Added new files and RL implementation

---
+++ scarl/aig_feature_extractor.py
@@ -0,0 +1,102 @@
+# BSD 3-Clause License
+
+# Copyright (c) 2024, NYU Machine-Learning guided Design Automation (MLDA)
+# Author: Animesh Basak Chowdhury
+# Date: March 9, 2024
+
+import torch
+import torch.nn as nn
+from gymnasium import spaces
+from stable_baselines3 import PPO
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from torch_geometric.nn import MessagePassing, aggr, SAGEConv,MultiAggregation,SoftmaxAggregation,BatchNorm
+import torch.nn.functional as F
+import torch as th
+
+allowable_features = {
+    'node_type' : [0,1,2],
+    'num_inverted_predecessors' : [0,1,2]
+}
+
+def get_node_feature_dims():
+    return list(map(len, [
+        allowable_features['node_type']
+    ]))
+
+full_node_feature_dims = get_node_feature_dims()
+
+class NodeEncoder(torch.nn.Module):
+
+    def __init__(self, emb_dim=3):
+        super(NodeEncoder, self).__init__()
+        self.node_type_embedding = torch.nn.Embedding(full_node_feature_dims[0], emb_dim)
+        torch.nn.init.xavier_uniform_(self.node_type_embedding.weight.data)
+
+    def forward(self, x):
+        # First feature is node type, second feature is inverted predecessor
+        x_embedding = self.node_type_embedding(x[:, 0])
+        x_embedding = torch.cat((x_embedding, x[:,1].reshape(-1,1)), dim=1)
+        #x_embedding = self.node_type_embedding(x)
+        return x_embedding
+
+class AIGStateEncoder(torch.nn.Module):
+    def __init__(self,node_encoder_out_channels,hidden_channels):
+        super().__init__()
+
+        self.node_encoder = NodeEncoder()
+        self.sage_aggr = MultiAggregation([SoftmaxAggregation(t=0.1,learn=True)])
+        self.conv1 = SAGEConv(node_encoder_out_channels,hidden_channels,aggr=self.sage_aggr)
+        self.conv2 = SAGEConv(hidden_channels,hidden_channels,aggr=self.sage_aggr)
+        self.norm1 = BatchNorm(hidden_channels)
+        self.norm2 = BatchNorm(hidden_channels)
+        # Use a global sort aggregation:
+        self.global_pool_1 = aggr.MLPAggregation(hidden_channels,hidden_channels)
+        self.global_pool_2 = aggr.GRUAggregation(hidden_channels,hidden_channels)
+        self.global_pool_3 = aggr.SetTransformerAggregation(hidden_channels,hidden_channels)
+        self.linear_layer = torch.nn.Linear(hidden_channels*3, hidden_channels*3)
+        self.features_dim = hidden_channels*3
+
+    def foward(self, x, edge_index, batch):
+        x = self.node_encoder(x)
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.relu(self.conv2(x, edge_index))
+        pooled_feature = torch.cat([self.global_pool(x, batch),self.global_pool_2(x,batch),self.global_pool_3(x,batch)],dim=1)
+        graph_feature = self.classifier(pooled_feature)
+        return graph_feature
+
+class CustomCombinedExtractor(BaseFeaturesExtractor):
+    def __init__(self, observation_space: spaces.Dict):
+        # We do not know features-dim here before going over all the items,
+        # so put something dummy for now. PyTorch requires calling
+        # nn.Module.__init__ before adding modules
+        super().__init__(observation_space, features_dim=1)
+
+        extractors = {}
+
+        total_concat_size = 0
+        # We need to know size of the output of this extractor,
+        # so go over all the spaces and compute output feature sizes
+        for key, subspace in observation_space.spaces.items():
+            if key == "image":
+                # We will just downsample one channel of the image by 4x4 and flatten.
+                # Assume the image is single-channel (subspace.shape[0] == 0)
+                extractors[key] = nn.Sequential(nn.MaxPool2d(4), nn.Flatten())
+                total_concat_size += subspace.shape[1] // 4 * subspace.shape[2] // 4
+            elif key == "vector":
+                # Run through a simple MLP
+                extractors[key] = nn.Linear(subspace.shape[0], 16)
+                total_concat_size += 16
+
+        self.extractors = nn.ModuleDict(extractors)
+
+        # Update the features dim manually
+        self._features_dim = total_concat_size
+
+    def forward(self, observations) -> th.Tensor:
+        encoded_tensor_list = []
+
+        # self.extractors contain nn.Modules that do all the processing.
+        for key, extractor in self.extractors.items():
+            encoded_tensor_list.append(extractor(observations[key]))
+        # Return a (B, self._features_dim) PyTorch tensor, where B is batch dimension.
+        return th.cat(encoded_tensor_list, dim=1)
--- scarl/main.py
+++ scarl/main.py
@@ -14,9 +14,29 @@ from config import options
 from utilities import log
 import argparse
 from utilities import Logger
+import gymnasium as gym
+from PPO import PPO
+from Synthesizor import Synthesizor
 
+def RLTrainer(args,logFile):
+    synthesis_env = Synthesizor(args,logFile)
+    rl_model = PPO("AIGPolicy",synthesis_env,verbose=1)
+    rl_model.learn(total_timesteps=args.num_timesteps)
+    rl_model.save("scarl_ppo_model")
+    
+def RLEvaluator(args,logFile):
+    rl_model = PPO.load("scarl_ppo_model")
+    synthesis_env = Synthesizor(args,logFile)
+    state, _ = synthesis_env.reset()
+    terminated=False
+    while not terminated:
+        action, _states = rl_model.predict(state)
+        state, reward, terminated, _, _ = synthesis_env.step(action)
+    print(f"Final Reward: {reward}")    
 
 if __name__ == '__main__':
     args = options().parse_args()
     yaml.load(args.params,Loader=yaml.FullLoader)
     logFile = Logger(args)
+    RLTrainer(args,logFile)
+    RLEvaluator(args,logFile)
